3ecda6ed048aa52bc1293777bc886d50d206c2ad
Travis <travwilliams@gmail.com>
2024-08-19 17:45:27 -0400

    change app to application

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 import os
 import re
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # Initialize OpenAI client
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
